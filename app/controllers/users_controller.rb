a7b30991cb0cb193444db9a92c5d4830da994297
dawidl022 <domdaw7@gmail.com>
2022-01-13 15:36:32 +0000

    Add error message displaying

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :edit, :show]
+  resources :users, except: [:destroy, :index]
 end
